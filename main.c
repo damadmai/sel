390b342751319d51d8d444c1251bff2a20188fde
Daniel A. Maierhofer <git@damadmai.at>
2017-04-08 11:25:29 +0200

    Add loop
    Remove switch

--- main.c
+++ main.c
@@ -3,16 +3,25 @@
 #include <stdint.h>
 #include <math.h>
 
+void evenodd(int zahl);
+
 int main(int argc, char *argv[])
 {
-    printf("Anzahl: %i Arg: %s\n", argc, argv[1]);
-    uint8_t zahl = atoi(argv[1]);
+    int i;
+    for (i = 10; i >= 0; i--)
+    {
+        printf("%i ", i);
+        evenodd(i);
+    }
+    printf("%i ", i);
+    return 0;
+}
 
-    printf("Zahl: %i Divsion: %i Rest: %i\n", zahl, zahl / 2, zahl % 2);
-
+void evenodd(int zahl)
+{
     if (zahl != 0)
     {
-        printf("nicht null\n");
+        printf("nicht null ");
         if (zahl % 2 == 1)
         {
             printf("ungerade\n");
@@ -22,26 +31,4 @@ int main(int argc, char *argv[])
             printf("gerade\n");
         }
     }
-
-    switch(zahl % 2)
-    {
-    case 0:
-        printf("Null\n");
-        break;
-    case 1:
-        printf("Eins\n");
-        break;
-    case 2:
-        printf("Zwei\n");
-        break;
-    case 3:
-        printf("Drei\n");
-        break;
-    default:
-        printf("Anderes\n");
-        break;
-    }
-
-    return 0;
 }
-
