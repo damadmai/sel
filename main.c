e353c9313fe5c5e49cdefaf6491a2b5202567b03
Daniel A. Maierhofer <git@damadmai.at>
2017-04-08 16:22:04 +0200

    Add file handling

--- main.c
+++ main.c
@@ -1,34 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <math.h>
-
-void evenodd(int zahl);
 
 int main(int argc, char *argv[])
 {
-    int i;
-    for (i = 10; i >= 0; i--)
-    {
-        printf("%i ", i);
-        evenodd(i);
-    }
-    printf("%i ", i);
-    return 0;
-}
+    FILE *fp;
 
-void evenodd(int zahl)
-{
-    if (zahl != 0)
+    fp = fopen("test.csv", "r");
+    if (fp == NULL)
     {
-        printf("nicht null ");
-        if (zahl % 2 == 1)
-        {
-            printf("ungerade\n");
-        }
-        else
-        {
-            printf("gerade\n");
-        }
+        fprintf(stderr, "Datei nicht geöffnet.\n");
+        exit(EXIT_FAILURE);
     }
+    for (int i = 10; i >= 0; i--)
+    {
+        fprintf(fp, "%i ", i);
+    }
+    fclose(fp);
+
+    return 0;
 }
