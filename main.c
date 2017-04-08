668e42bb37f098d5cae6a7eb5905a4bdc8ca2f19
Daniel A. Maierhofer <git@damadmai.at>
2017-04-08 11:17:46 +0200

    Add even odd detection

--- main.c
+++ main.c
@@ -1,38 +1,47 @@
-/*  calc4.c  */
-/*  four-function calculator  */
 #include <stdio.h>
-main()
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+
+int main(int argc, char *argv[])
 {
-     float num1,num2;
-     char op;
+    printf("Anzahl: %i Arg: %s\n", argc, argv[1]);
+    uint8_t zahl = atoi(argv[1]);
 
-     while(1)
-          {
-          printf("Type number, operator, number\n");
-          scanf("%f %d %f",&num1,&op,&num2);
-          switch(op)
-               {
-               case'+':
-                    printf(" = %f",num1+num2);
-                    break;
-               case'-':
-                    printf(" = %f",num1-num2);
-                    break;
-               case'*':
-               case'x':
-                    printf(" = %f",num1*num2);
-                    break;
-               case'/':
- /*              case':':
-               case'\':   */
-                    printf(" = %f",num1/num2);
-                    break;
-               default:
-                    printf("Unknown operator");
-               }
-          printf("\n");
-          return 0;
-          }
+    printf("Zahl: %i Divsion: %i Rest: %i\n", zahl, zahl / 2, zahl % 2);
+
+    if (zahl != 0)
+    {
+        printf("nicht null\n");
+        if (zahl % 2 == 1)
+        {
+            printf("ungerade\n");
+        }
+        else
+        {
+            printf("gerade\n");
+        }
+    }
+
+    switch(zahl % 2)
+    {
+    case 0:
+        printf("Null\n");
+        break;
+    case 1:
+        printf("Eins\n");
+        break;
+    case 2:
+        printf("Zwei\n");
+        break;
+    case 3:
+        printf("Drei\n");
+        break;
+    default:
+        printf("Anderes\n");
+        break;
+    }
+
+    return 0;
 }
-
 
